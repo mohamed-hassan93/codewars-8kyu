04abfbdf3a5901d56b48ba7a8885dceba1cb3d24
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-04-17 22:42:00 -0400

    new codewar entries

--- codewars-8kyu/js/main.js
+++ codewars-8kyu/js/main.js
@@ -158,3 +158,50 @@ function squareSum(numbers){
   }, 0);
   return newNew
 }
+
+// 8 kyu - Short to long 
+
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+// Hint for R users:
+
+// The length of string is not always the same as the number of characters
+// For example: (Input1, Input2) --> output
+
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+
+
+
+function solution(a, b){
+  if(a.length<b.length){
+    return newStr = a+b+a
+  }else{
+    return newStr = b+a+b
+  }
+}
+
+// 8kyu - Name Shuffler 
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// name_shuffler('john McClane'); => "McClane john"
+
+function nameShuffler(str){
+  let arr = str.split(" ");
+  let newStr = `${arr[1]} ${arr[0]}`;
+  return newStr
+}
+
+// 8kyu - Capitalization and Mutability 
+
+// Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+
+// Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
+
+// Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
+
+function capitalizeWord(word) {
+  let newWord = word[0].toUpperCase()+word.slice(1);
+  return newWord;
+}
