43f6892b2f7a277ac4fd8311b6bd5287a6ea5f4c
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-04-30 16:11:15 -0400

    new entries

--- js/main.js
+++ js/main.js
@@ -213,3 +213,48 @@ function capitalizeWord(word) {
     let newWord = word[0].toUpperCase()+word.slice(1);
     return newWord;
   }
+
+  //Arrays
+//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+
+let pokeList = 'one, two, three, four, five';
+
+function listReverse(pokelist){
+    let arr = pokelist.split(', ');
+    let newArr = arr.reverse();
+    let newList = newArr.join(' ')
+    console.log(newList)
+}
+
+listReverse(pokeList)
+
+//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of 
+// each element in b.
+
+function compareSquareAndCube(a,b){
+    console.log(a.reduce((acc,c)=> acc + c**2, 0) > b.reduce((acc,c)=> acc + c**3, 0))
+}
+
+compareSquareAndCube([2,2,2], [2,2,2])
+
+
+//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+// Some cases:
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+// [68, -1, 1, -7, 10, 10] => [-1, 10];
+
+function evenDivide(arr){
+    console.log(arr.filter((e,i) => e%i===0))
+}
+
+evenDivide([22, -6, 32, 82, 9, 25])
+
+
+
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+
+function sumOfValues(arr){
+    console.log(arr.reduce((acc,c) => acc + Number(c), 0))
+}
+
+sumOfValues([3,'4','5',6,9])
