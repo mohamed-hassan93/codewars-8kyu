36ca97f48faf760a89480cfff1806807c08c6f43
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-05-24 23:23:22 -0500

    new code war

---
+++ codewars-8kyu/Code Wars - 23May2022/js/main.js
@@ -0,0 +1,10 @@
+// 8 kyu - convert a boolean to a string 
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+  return b.toString()
+}
+
