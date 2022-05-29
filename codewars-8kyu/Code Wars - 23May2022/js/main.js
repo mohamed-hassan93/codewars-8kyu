fb1860940d7a07ee2562685cc0f24d9802e19871
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-05-28 23:44:47 -0400

    new codewar 28May2022

--- codewars-8kyu/Code Wars - 23May2022/js/main.js
+++ codewars-8kyu/Code Wars - 23May2022/js/main.js
@@ -1,10 +1,33 @@
-// 8 kyu - convert a boolean to a string 
+// 7 kyu - You're a square!
 
-// Implement a function which convert the given boolean value into its string representation.
+// A square of squares
+// You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
 
-// Note: Only valid inputs will be given.
+// However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
 
-function booleanToString(b){
-  return b.toString()
+// Task
+// Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+// Examples
+// -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+// 25  =>  true
+// 26  =>  false
+
+var isSquare = function(n){
+  if((n**0.5)%1 === 0){
+    return true
+  }else{
+    return false
+  }
 }
+
+
+
 
