ae066be7cfbf25abf60106b3120b1ed0adf5a868
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-06-11 09:35:01 -0400

    new

--- codewars-8kyu/06Jun2022/Code Wars - 11June2022/js/main.js
+++ codewars-8kyu/06Jun2022/Code Wars - 11June2022/js/main.js
@@ -1,11 +1,36 @@
-// 8 kyu - Array plus array
+// 8 kyu - Merge two sorted arrays into one
 
-// I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+// You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
 
-// P.S. Each array includes only integer numbers. Output is a number too.
+// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+// Examples (input -> output)
+// * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+// Happy coding!
+
+function mergeArrays(arr1, arr2) {
+  let arr3 = arr1.concat(arr2);
+  let arr4 = arr3.sort((a,b) => a-b)
+  return arr4.filter((obj,index) => arr4.indexOf(obj) === index);
+}
 
 
-function arrayPlusArray(arr1, arr2) {
-  let arr3 = arr1.concat(arr2)
-  return arr3.reduce((sum, current) => sum + current, 0); //something went wrong
+// 8 Kyu - Get the mean of an array
+
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+function getAverage(marks){
+  let sum = marks.reduce((acc,obj) => acc+obj, 0).toFixed();
+  let ave = sum/marks.length;
+  return Number(Math.floor(ave))
 }
