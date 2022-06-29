dc21c454d6c9172f8ebba72d87d4fda3ae3d2af8
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-06-28 23:47:41 -0400

    new codewar

--- codewars-8kyu/06Jun2022/Code Wars - 23June2022/js/main.js
+++ codewars-8kyu/06Jun2022/Code Wars - 23June2022/js/main.js
@@ -16,3 +16,69 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   }
 };
 
+
+const arr = [1,2,3,4,5,6,7,8,9,10];
+
+function swap(arr, i, j) {
+  const temp = arr[i];    // 
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+// arr.length = N   O(N/2) = O(N) => time complexity is linear
+// SPACE COMPLEXITY = N
+// SPACE COMPLEXITY = N IN => 2N OUT => O(2N) => O(N)
+// don't use Array.reverse, we want to implement reverse ourselves ;)
+// reverse inline (not copy to reduce complexity)
+function reverseArray(arr) {
+  const middleIndex = Math.floor(arr.length / 2);
+  for(let i = 0; i < middleIndex; i++){
+    swap(arr, i, arr.length - 1 - i)
+ }
+  return arr
+}
+
+const reversed = reverseArray(arr);
+if(reversed[0] !== 10) {
+  console.error("Meeeeh")
+}
+
+
+
+// [10,9,8,7,6,5,4,3,2,1]
+
+const newEmail1 = {
+  to: "email@something.com",
+  title: "sduhfüasdufasd",
+  body: "uhsdpifuhspdfas",
+}
+
+const newEmail2 = {
+  to: "email@something.com",
+  title: "sduhfüasdufasd",
+  body: "uhsdpifuhspdfas",
+}
+
+const emailHandler = new EmailHandler();
+
+emailHandler.push(newEmail1);
+emailHandler.push(newEmail2);
+
+
+const nextEmail1 = emailHandler.processNext();  // newEmail1
+const nextEmail2 = emailHandler.processNext();  // newEmail2
+
+
+class EmailHandler {
+  constructor() {
+    this.array = [];
+  }
+  
+  push(email) {
+    this.array.push(email);
+  }
+  
+  processNext() {
+    return this.array.unshift();
+  }
+}
