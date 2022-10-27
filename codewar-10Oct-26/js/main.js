09a6fb561fdd820e5cf1038db936e90d552bd36e
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-10-27 01:02:08 -0400

    daily codewar entry

---
+++ codewar-10Oct-26/js/main.js
@@ -0,0 +1,10 @@
+// 8 kyu - MakeUpperCase
+
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+  // used toUpperCase() method to return an uppercased string
+  return str.toUpperCase()
+}
+
+}
