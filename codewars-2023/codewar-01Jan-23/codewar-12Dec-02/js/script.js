228328c6119916424c477a19c0ca129dbbfa67d9
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2023-01-01 23:48:04 -0500

    new code war 01Jan2022 and file restructure

---
+++ codewars-2023/codewar-01Jan-23/codewar-12Dec-02/js/script.js
@@ -0,0 +1,21 @@
+// // 8 kyu - Are You Playing Banjo?
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+
+function areYouPlayingBanjo(name) {
+  // Implement me
+  let nameBreak = name.split('');
+  if(nameBreak[0] === 'R' || nameBreak[0] === 'r'){
+    return `${name} plays banjo`
+  }else{
+    return `${name} does not play banjo`
+  }
+}
